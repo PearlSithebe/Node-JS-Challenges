5261078f646266eb109bb84fa6f2de87342021da
PearlSithebe <pearlsithebe@gmail.com>
2019-07-29 22:37:55 +0200

    code modification

--- Node&FileIO/visitorlog.js
+++ Node&FileIO/visitorlog.js
@@ -1,25 +1,34 @@
-let count =  1;
+let visitorArray = [];
+
 class Visitor {
     
-    getDetails(fullName, age, date, time, comment, nameOfAssistant) {
-        
-        return {
-        fullName,
-        age,
-        date,
-        time,
-        comment, 
-        nameOfAssistant
-        }
-    }
+getDetails(fullName, age, date, time, comment, nameOfAssistant) {
     
-    save (getDetails){
-        let visitorData = getDetails;
-        const fs = require('fs');
-        let data = JSON.stringify(visitorData);
-        if(
-        fs.writeFileSync(`visitor_${count}.json`, data, function() {})){
-        
-        }
+    return {
+    fullName,
+    age,
+    date,
+    time,
+    comment, 
+    nameOfAssistant
     }
 }
+ 
+save (getDetails){
+    visitorArray.push(getDetails)
+    const fs = require('fs');
+    for(var i=0; i < visitorArray.length; i++){
+    let data = JSON.stringify(visitorArray[i])
+    fs.writeFileSync(`visitor_${i +1}.json`, data, function() {})
+    console.log("saved")
+   
+    }
+    
+}  
+
+load(i){
+    
+   return require(`./visitor_${i}.json`)
+    
+
+}}
---
+++ README.txt.txt
@@ -0,0 +1,6 @@
+> PROJECTS: Node & File IO
+
+> PROJECTS: Node & mongoDB assignment
+> PROJECTS: Express, forms and templates
+> PROJECTS: Expose a JSON API
+> PROJECTS: Add a little Ajax
